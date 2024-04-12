91136eed153f8ffe25f4674ea023242993ac49e9
Julian Wiesler <wiesleju@gmail.com>
2024-04-12 12:54:36 +0200

    Logging

--- src/publish.ts
+++ src/publish.ts
@@ -5,7 +5,7 @@ import { cargoExecutable, exec, PluginConfig, SemanticReleaseError } from './uti
  * Publish the current package to the crate registry.
  */
 export default async ({ executable, allFeatures = false, publishArgs = [] }: PluginConfig, { logger }: Context) => {
-  logger.info('Publish cargo package.');
+  logger.log('Publish cargo package.');
   if (allFeatures && !publishArgs.includes('--all-features')) {
     publishArgs.push('--all-features');
   }
@@ -16,6 +16,8 @@ export default async ({ executable, allFeatures = false, publishArgs = [] }: Plu
 
   const { stderr, exitCode } = await exec(cargoExecutable(executable), ['publish', ...publishArgs]);
   if (exitCode !== 0) {
+    logger.error(`Cargo publish failed: ${stderr}`);
+    logger.error(new SemanticReleaseError('Cargo publish failed', 'ECARGOPUBLISH', stderr));
     throw new SemanticReleaseError('Cargo publish failed', 'ECARGOPUBLISH', stderr);
   }
 };
